32650f040508841878563fc480db07b2dcecbd75
christopherfick <fick.christopher@yahoo.com>
2022-08-31 11:08:09 +0200

    Create data directory if none exists

--- yify_collect_data/main.py
+++ yify_collect_data/main.py
@@ -4,17 +4,24 @@ from tqdm import tqdm
 
 import time
 
+import os
+from pathlib import Path
+
+# Create data dir if none exists
+data = Path(os.path.realpath(__file__)).parents[0]/'data'
+if not os.path.exists(data):
+    os.mkdir(data)
 
 # Instantiate objects to help with management of databases
-href_table = href_table("data/href.db", "href")
-movie_table = MovieTable("data/movie.db", "movie")
+href_table = href_table(data/"href.db", "href")
+movie_table = MovieTable(data/"movie.db", "movie")
 
 # # Load initial hrefs from 100 yiffy pages, if href database empty
-# href_table.load_hrefs(page_end = 100)
+href_table.load_hrefs(page_end = 100)
 
 # # Load movie data from hrefs in href database
-# hrefs = href_table.fetch_hrefs()
-# movie_table.load_movies(hrefs)
+hrefs = href_table.fetch_hrefs()
+movie_table.load_movies(hrefs)
 
 
 # # Once databases loaded, Update (if any) with latest released content
